01de38ca1d44a72508c6c69273c272fa9c396755
刘家玮 <2521747947@qq.com>
2025-04-23 19:34:31 +0800

    feat: gateway实现redis拉取权限信息

--- sf-club-gateway/src/main/java/com/jingdiansuifeng/club/gateway/auth/StpInterfaceImpl.java
+++ sf-club-gateway/src/main/java/com/jingdiansuifeng/club/gateway/auth/StpInterfaceImpl.java
@@ -1,9 +1,13 @@
 package com.jingdiansuifeng.club.gateway.auth;
 
 import cn.dev33.satoken.stp.StpInterface;
+import com.alibaba.nacos.api.utils.StringUtils;
+import com.google.gson.Gson;
+import com.jingdiansuifeng.club.gateway.redis.RedisUtil;
 import org.springframework.stereotype.Component;
 
-import java.util.LinkedList;
+import javax.annotation.Resource;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -12,20 +16,33 @@ import java.util.List;
 @Component
 public class StpInterfaceImpl implements StpInterface {
 
+    @Resource
+    private RedisUtil redisUtil;
+
+    private String authPermissionPrefix = "auth:permission:";
+
+    private String authRolePrefix = "auth:role:";
+
     @Override
     public List<String> getPermissionList(Object loginId, String loginType) {
-        // 返回此 loginId 拥有的权限列表
-        List<String> permissList = new LinkedList<>();
-        permissList.add("subject:add");
-        return permissList;
+        // 返回拥有的权限列表
+        return getAuth(loginId.toString(),authPermissionPrefix);
     }
 
     @Override
     public List<String> getRoleList(Object loginId, String loginType) {
-        // 返回此 loginId 拥有的角色列表
-        List<String> roleList = new LinkedList<>();
-        roleList.add("admin");
-        return roleList;
+        return getAuth(loginId.toString(),authRolePrefix);
+    }
+
+    private List<String> getAuth(Object loginId,String prefix) {
+        // 返回拥有的角色列表
+        String authKey = redisUtil.buildKey(prefix, loginId.toString());
+        String authValue = redisUtil.get(authKey);
+        if (StringUtils.isBlank(authValue)){
+            return Collections.emptyList();
+        }
+        List<String> authList = new Gson().fromJson(authValue, List.class);
+        return authList;
     }
 
 }
