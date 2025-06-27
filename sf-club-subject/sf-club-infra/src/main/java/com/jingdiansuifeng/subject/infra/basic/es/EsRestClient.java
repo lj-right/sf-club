12138b1d0a04d777136d9516b111857ca8dc4536
刘家玮 <2521747947@qq.com>
2025-06-27 19:16:07 +0800

    自定义封装集群es连接统一管理

---
+++ sf-club-subject/sf-club-infra/src/main/java/com/jingdiansuifeng/subject/infra/basic/es/EsRestClient.java
@@ -0,0 +1,58 @@
+package com.jingdiansuifeng.subject.infra.basic.es;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHost;
+import org.apache.http.client.methods.HttpPost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+@Slf4j
+public class EsRestClient {
+
+    public static Map<String, RestHighLevelClient> clientMap = new HashMap<>();
+
+    @Resource
+    private EsConfigProperties esConfigProperties;
+
+    @PostConstruct
+    public void initialize() {
+        List<EsClusterConfig> esConfigs = esConfigProperties.getEsConfigs();
+        for (EsClusterConfig esConfig : esConfigs) {
+            log.info("initialize.config.name:{},node:{}", esConfig.getName(), esConfig.getNodes());
+            RestHighLevelClient restHighLevelClient = initRestClient(esConfig);
+            if (restHighLevelClient != null) {
+                clientMap.put(esConfig.getName(), restHighLevelClient);
+            } else {
+                log.error("config.name:{},node:{},init.error", esConfig.getName(), esConfig.getNodes());
+            }
+        }
+    }
+
+    private RestHighLevelClient initRestClient(EsClusterConfig esClusterConfig) {
+        String[] ipPortArr = esClusterConfig.getNodes().split(",");
+        List<HttpHost> httpHostList = new ArrayList<>(ipPortArr.length);
+        for (String ipPort : ipPortArr) {
+            String[] ipPortInfo = ipPort.split(":");
+            if (ipPortInfo.length == 2) {
+                HttpHost httpHost = new HttpHost(ipPortInfo[0], Integer.parseInt(ipPortInfo[1]));
+                httpHostList.add(httpHost);
+            }
+        }
+        HttpHost[] httpHosts = new HttpHost[httpHostList.size()];
+        httpHostList.toArray(httpHosts);
+
+        RestClientBuilder builder = RestClient.builder(httpHosts);
+        return new RestHighLevelClient(builder);
+    }
+
+}
