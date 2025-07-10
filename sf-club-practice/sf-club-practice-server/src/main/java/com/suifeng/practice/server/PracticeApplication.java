62c9b1760d8e072ecbd541ceef099415935001f9
刘家玮 <2521747947@qq.com>
2025-07-10 18:26:32 +0800

    feat:练题微服务架构搭建

---
+++ sf-club-practice/sf-club-practice-server/src/main/java/com/suifeng/practice/server/PracticeApplication.java
@@ -0,0 +1,21 @@
+package com.suifeng.practice.server;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * 练习微服务启动类
+ *
+ */
+
+@SpringBootApplication
+@ComponentScan("com.suifeng.practice")
+@MapperScan("com.suifeng.**.mapper")
+public class PracticeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeApplication.class);
+
+    }
+}
