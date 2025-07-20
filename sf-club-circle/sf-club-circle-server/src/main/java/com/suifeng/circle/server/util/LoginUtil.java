ae487f2318073433cbc7cb55526c50344caeec5c
刘家玮 <2521747947@qq.com>
2025-07-20 20:15:13 +0800

    feat: 交流圈微服务搭建

---
+++ sf-club-circle/sf-club-circle-server/src/main/java/com/suifeng/circle/server/util/LoginUtil.java
@@ -0,0 +1,19 @@
+package com.suifeng.circle.server.util;
+
+
+import com.suifeng.circle.server.config.context.LoginContextHolder;
+
+/**
+ * 用户登录util
+ *
+ * @author: ChickenWing
+ * @date: 2023/11/26
+ */
+public class LoginUtil {
+
+    public static String getLoginId() {
+        return LoginContextHolder.getLoginId();
+    }
+
+
+}
