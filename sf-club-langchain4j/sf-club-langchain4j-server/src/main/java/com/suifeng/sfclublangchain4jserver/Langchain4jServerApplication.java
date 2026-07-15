3497a66ef6893f4db4e28e4ae22a5fc74278984c
suifeng <2521747947@qq.com>
2026-07-15 17:55:43 +0800

    引入langchain4j

---
+++ sf-club-langchain4j/sf-club-langchain4j-server/src/main/java/com/suifeng/sfclublangchain4jserver/Langchain4jServerApplication.java
@@ -0,0 +1,13 @@
+package com.suifeng.sfclublangchain4jserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Langchain4jServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Langchain4jServerApplication.class, args);
+    }
+
+}
---
+++ sf-club-langchain4j/sf-club-langchain4j-server/src/test/java/com/suifeng/sfclublangchain4jserver/SfClubLangchain4jServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suifeng.sfclublangchain4jserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfClubLangchain4jServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
