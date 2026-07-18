ce924484f2eb51a64c68a5499d20ec534ba06915
suifeng <2521747947@qq.com>
2026-07-18 19:40:33 +0800

    feat: 添加结构化输出

--- sf-club-langchain4j/sf-club-langchain4j-server/src/main/java/com/suifeng/sfclublangchain4jserver/ai/AiCodeHelperService.java
+++ sf-club-langchain4j/sf-club-langchain4j-server/src/main/java/com/suifeng/sfclublangchain4jserver/ai/AiCodeHelperService.java
@@ -1,11 +1,21 @@
 package com.suifeng.sfclublangchain4jserver.ai;
 
 
+import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.service.spring.AiService;
+
+import java.util.List;
 
 //@AiService
 public interface AiCodeHelperService {
     @SystemMessage(fromResource = "system-prompt.txt")
-    String chat(String userMsg);
+    String chat(@MemoryId int memoryId,@UserMessage String userMsg);
+
+
+    @SystemMessage(fromResource = "system-prompt.txt")
+    Report chatForReport(@MemoryId int memoryId,@UserMessage String userMsg);
+
+    record Report(String name, List<String> suggestionList){};
 }
--- sf-club-langchain4j/sf-club-langchain4j-server/src/test/java/com/suifeng/sfclublangchain4jserver/ai/AiCodeHelperServiceTest.java
+++ sf-club-langchain4j/sf-club-langchain4j-server/src/test/java/com/suifeng/sfclublangchain4jserver/ai/AiCodeHelperServiceTest.java
@@ -16,15 +16,23 @@ class AiCodeHelperServiceTest {
 
     @Test
     void chat() {
-        String resultMeassage = aiCodeHelperService.chat("你好，我是随风");
+        String resultMeassage = aiCodeHelperService.chat(1,"你好，我是随风");
         System.out.println(resultMeassage);
     }
 
     @Test
     void chatWithMessage() {
-        String result = aiCodeHelperService.chat("你好，我是随风");
+        String result = aiCodeHelperService.chat(1,"你好，我是随风");
         System.out.println(result);
-        result = aiCodeHelperService.chat("我是谁来着？小民吗？");
+        result = aiCodeHelperService.chat(1,"我是谁来着？我是小明还是小美");
         System.out.println(result);
+    }
+
+    @Test
+    void chatForReport() {
+        String useMessage = "你好，我是随风，喜欢唱跳rap篮球，请帮我制定一周编程刷题计划";
+        AiCodeHelperService.Report report = aiCodeHelperService.chatForReport(1, useMessage);
+        System.out.println(report);
+
     }
 }
